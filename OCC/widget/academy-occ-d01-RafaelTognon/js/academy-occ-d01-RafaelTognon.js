cf0c25840c8b506758bb5f1ef125c7ccc48c9a96
Rafael Tognon <rafaelrovanitognon@gmail.com>
2021-03-05 18:02:24 -0300

    Desafio 1 finalizado

---
+++ OCC/widget/academy-occ-d01-RafaelTognon/js/academy-occ-d01-RafaelTognon.js
@@ -0,0 +1,8 @@
+define(
+  ['jquery', 'knockout'],
+  function($,ko) {
+    'use strict';
+  return {
+      textInput: ko.observable("Banner rotativo com 4 imagens")
+  }
+});
---
+++ academy-occ-d01-RafaelTognon/widget/academy-occ-d01-RafaelTognon/js/academy-occ-d01-RafaelTognon.js
@@ -0,0 +1,19 @@
+define(
+  // Dependencies
+  ['jquery', 'knockout'],
+  // Module Implementation
+  function($,ko) {
+    // We recommend enabling strict checking mode
+    'use strict';
+    // Private variables and functions can be defined here...
+  return {
+  // Widget JS
+      // Some member variables...
+      textInput: ko.observable(),
+      // Some public methods...
+      doSomethingWithInput: function () {
+    //...
+  }
+  }
+});
+ 
